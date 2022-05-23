c4d0192de098d22402d3cb9e022e8bc38a7b4cd3
“kmcaloney15” <Katherine@Katys-MBP-2.hsd1.ma.comcast.net>
2022-05-23 14:50:00 -0400

    got books to flip with a hover, however now need to make it so they flip individually and make the action on click

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 
 // Global Variables --------------------------------------------------------------------------
 //declair books and boxes
-
+books = document.querySelector('.books');
+gameBoard = document.querySelector('.gameboard');
 
 
 // Click addEventListener - for flipping the books -----------------------------------------------------------------------------
 
 //want to use classes to distinguise which pair matches with witch. for and if loops? 
+
+//want to try to randomize which books get places where so each game is different. Possibly make them into an array and use math.floor(math.random);
+
 
 
 
@@ -16,7 +20,7 @@
 
 
 // If there is a mis-match - trigger phone noise and deduct a life (maybe in top corner?)
-
+// -how to keep track of misses - should I keep track of the number of flips that don't trigger the win condition?
 
 
 
