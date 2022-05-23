eaf20c2d03a2a4a20410599c0b050e2fe115090f
“kmcaloney15” <Katherine@Katys-MBP-2.hsd1.ma.comcast.net>
2022-05-23 09:32:24 -0400

    starting pseudocode

---
+++ main.js
@@ -0,0 +1,28 @@
+
+// Global Variables --------------------------------------------------------------------------
+//declair books and boxes
+
+
+
+// Click addEventListener - for flipping the books -----------------------------------------------------------------------------
+
+//want to use classes to distinguise which pair matches with witch. for and if loops? 
+
+
+
+// Win & Loose Conditionals -----------------------------------------------------------------------------
+
+// When two books are matched, have them dissapear - use classes and .remove
+
+
+// If there is a mis-match - trigger phone noise and deduct a life (maybe in top corner?)
+
+
+
+
+// End Game - not sure if needs to be seperate from Win/Lose -----------------------------------------------------------------------------
+
+// Maybe have game clear and lose page appear.
+
+
+//-----------------------------------------------------------------------------
