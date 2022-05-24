4504f67055e4bd31eca05f80cb6eef13c1d96f38
“kmcaloney15” <Katherine@Katys-MBP-2.hsd1.ma.comcast.net>
2022-05-24 15:15:08 -0400

    toggle function worked to flip the book on click.

--- main.js
+++ main.js
@@ -1,72 +1,25 @@
-// Global Variables --------------------------------------------------------------------------/
+const books = document.querySelectorAll(".book");
 
-//declair books and boxes
-books = document.querySelectorAll(".book");
-gameBoard = document.querySelector(".gameboard");
-let frontImage = document.querySelector(".front");
-let title;
-// let backFaceImage = document.querySelector('.back-face');
-// console.log(books);
-let bookParent;
-// console.log(backFaceImage)
-//  -------------------------- Event Listeners -------------------------------------------/
+let hasFlippedBook = false;
+let closedBook;
+let bookTitle;
 
-
-for (const book of books) {
-    book.addEventListener("click", playerClick);
-    console.log(bookParent)
+function flipBook() {
+  this.classList.toggle("flip");
+    // console.log('this was clicked')
+  //if hasFlippedcard is false - then it's the first time a player has clicked the book
+//   if (!hasFlippedBook) {
+//     //first click
+//     hasFlippedBook = true;
+//     closedBook = this;
+//     console.log({hasFlippedBook, closedBook})
+//   } else {
+//       hasFlippedBook = false;
+//       bookTitle = this;
+//   }
 }
 
 
-
-//once they click - event listener - set timeout where if the win is false toggle the action that flipped the card in the first place - flipBookAction
-
-
-//-----------------FUNCTIONS------------------------------------/
-
-function playerClick() {
-    book.classList.add("flipBookAction");
-    // console.log(book);
-    
-    //value always has to be a string
-    title = document.querySelector(".back :nth-child(1)");
-    bookParent = book.children[1];
-    // console.log(bookParent.children[0].innerHTML);
-    let bookName = title.innerText;
-    // console.log(title);
-    // console.log(bookParent.classList);
-    
-
-    setTimeout(timedFlip, 550);
-
-    // console.log('this is working');
-}
-
-
-function timedFlip() {
-  frontImage.style.visibility = "hidden";
-  title.style.visibility = "visible";
-}
-
-
-//want to try to randomize which books get places where so each game is different. Possibly make them into an array and use math.floor(math.random);
-
-// Win & Loose function
-
-
-//pop last number from id and then say if id = id - then win
-
-
-
-
-// When two books are matched, have them dissapear - use classes and .remove
-
-// If there is a mis-match - trigger phone noise and deduct a life (maybe in top corner?)
-// -how to keep track of misses - should I keep track of the number of flips that don't trigger the win condition?
-
-// End Game - not sure if needs to be seperate from Win/Lose -----------------------------------------------------------------------------
-
-// Maybe have game clear and lose page appear.
-
-//-----------------------------------------------------------------------------
-
+books.forEach(book => {
+     book.addEventListener('click', flipBook)
+    });
