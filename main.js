51fe96152d1c392678e3e734e780b30908b65d1c
“kmcaloney15” <Katherine@Katys-MBP-2.hsd1.ma.comcast.net>
2022-05-24 15:35:57 -0400

    Removed excess code and got the book titles to display when the book is flipped

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
+// ------------ Global Variables ---------------------------------------/
+
 const books = document.querySelectorAll(".book");
 
 let hasFlippedBook = false;
 let closedBook;
 let bookTitle;
 
+//---------------------- FUNCTIONS ------------------------------------/
+
 function flipBook() {
-  this.classList.toggle("flip");
+  this.classList.add("flip");
     // console.log('this was clicked')
   //if hasFlippedcard is false - then it's the first time a player has clicked the book
-//   if (!hasFlippedBook) {
-//     //first click
-//     hasFlippedBook = true;
-//     closedBook = this;
-//     console.log({hasFlippedBook, closedBook})
-//   } else {
-//       hasFlippedBook = false;
-//       bookTitle = this;
-//   }
+  if (hasFlippedBook === false) {
+    //first click
+    hasFlippedBook = true;
+    closedBook = this;
+    console.log({hasFlippedBook, closedBook})
+  } else {
+      //
+      hasFlippedBook = false;
+      bookTitle = this;
+  }
 }
+
+//  -------------------------- Event Listeners -------------------------/
 
 
 books.forEach(book => {
      book.addEventListener('click', flipBook)
     });
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
